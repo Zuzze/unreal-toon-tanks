0b371ab91d977b05f7f3ef8f5d590bb6dcf6ec32
Susanna Nevalainen <susanna.nevalainen@oysterlab.ch>
2020-10-13 23:22:14 +0200

    tank moving direction and rotation logic added

--- Source/ToonTanks/Pawns/PawnTank.cpp
+++ Source/ToonTanks/Pawns/PawnTank.cpp
@@ -24,10 +24,43 @@ void APawnTank::BeginPlay()
 void APawnTank::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+
+    Rotate();
+    Move();
 }
 
 // Called to bind functionality to input
 void APawnTank::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
+    // Input mapping names from project settings
+    PlayerInputComponent->BindAxis("MoveForward", this, &APawnTank::CalculateMoveInput);
+    PlayerInputComponent->BindAxis("Turn", this, &APawnTank::CalculateRotateInput);
+}
+
+/* Return */
+void APawnTank::CalculateMoveInput(float Value)
+{
+    // move on x axis
+    // velocity = s = d / t = distance / time
+    // => d = distance = velocity * time = s * t
+    MoveDirection = FVector(Value * MoveSpeed * GetWorld()->DeltaTimeSeconds, 0, 0);
+}
+
+void APawnTank::CalculateRotateInput(float Value)
+{
+    float RotateAmount = Value * RotateSpeed * GetWorld()->DeltaTimeSeconds;
+    // rotation on yaw (z-axis) is same as rotation on the ground
+    FRotator Rotation = FRotator(0, RotateAmount, 0);
+    RotationDirection = FQuat(Rotation);
+}
+
+void APawnTank::Move()
+{
+    AddActorLocalOffset(MoveDirection, true);
+}
+
+void APawnTank::Rotate()
+{
+    AddActorLocalRotation(RotationDirection, true);
 }
